b2b1cd5148801e055d9cf5c6c57e577787fd7238
Drfin <vipinj444@gmail.com>
2020-01-02 16:16:20 +0530

    hello everyone

--- app/src/main/java/com/example/gitapplication/MainActivity.java
+++ app/src/main/java/com/example/gitapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gitapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // this is first Application of fin
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
