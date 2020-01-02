f45e4ca9944ac40c9df56436187f6e1a69e066af
Drfin <vipinj444@gmail.com>
2020-01-02 15:24:05 +0530

    Update_application

--- app/src/main/java/com/example/gitapplication/MainActivity.java
+++ app/src/main/java/com/example/gitapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // this is first Application of fin
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
